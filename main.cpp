747b4bf9060fa2ffc98234015cf627b56563175e
Kim Seungha <seungha.me@gmail.com>
2021-11-04 19:31:32 +0900

    move camera instead of object

--- main.cpp
+++ main.cpp
@@ -130,12 +130,14 @@ int main()
         auto ratio = (sin(timeValue) / 2.0f) + 0.5f;
         glm::mat4 model = glm::mat4(1.0f);
 //        model = glm::translate(model, glm::vec3(0.5f, 0.5f, 0.0f));
-        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
         model = glm::rotate(model, timeValue, glm::vec3(0.0f, 0.0f, 1.0f));
 //        model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.0f));
 
         glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        view = glm::translate(view, glm::vec3(0.0f, 2.0f, 2.0f));
+        view = glm::rotate(view, glm::radians(-45.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        view = glm::inverse(view);
 
         glm::mat4 projection;
         projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
