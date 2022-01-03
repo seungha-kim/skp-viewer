3d63a0334fdfdfe77a686bc65366f8c8861c1f74
Kim Seungha <seungha.me@gmail.com>
2022-01-03 16:48:39 +0900

    basic mouse handling of input controller

--- studio/input_controller.py
+++ studio/input_controller.py
@@ -1,9 +1,32 @@
 from abc import *
+from enum import Flag, auto
 from PySide6.QtCore import Qt
+
+
+class MouseButton(Flag):
+    NONE = 0
+
+    LEFT = auto()
+    MIDDLE = auto()
+    RIGHT = auto()
+
+    @classmethod
+    def from_qt_buttons(cls, buttons: Qt.MouseButtons) -> 'MouseButton':
+        result = cls.NONE
+        if buttons & Qt.LeftButton:
+            result |= cls.LEFT
+        if buttons & Qt.MiddleButton:
+            result |= cls.MIDDLE
+        if buttons & Qt.RightButton:
+            result |= cls.RIGHT
+        return result
 
 
 class AbstractInputController(ABC):
     def handle_key(self, key: Qt.Key, modifiers: Qt.KeyboardModifiers, pressed: bool) -> bool:
+        pass
+
+    def handle_mouse_move(self, x: int, y: int, width: int, height: int, buttons: MouseButton) -> None:
         pass
 
     def update(self) -> None:
@@ -23,6 +46,9 @@ class InputControllerOverriding(AbstractInputController):
                 self._overrider.handle_key(key, modifiers, pressed)
                 or self._overridden.handle_key(key, modifiers, pressed)
         )
+
+    def handle_mouse_move(self, x: int, y: int, width: int, height: int, buttons: MouseButton) -> None:
+        self._overrider.handle_mouse_move(x, y, width, height, buttons)
 
     def update(self) -> None:
         self._overridden.update()
