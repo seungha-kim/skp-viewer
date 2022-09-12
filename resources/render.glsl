e9bf364434bc1d61a0ec188ad465d09beaa2305d
Dan Kim <dan@acon3d.com>
2022-09-12 16:07:31 +0900

    workaround for flipped normals in skp

--- resources/render.glsl
+++ resources/render.glsl
@@ -240,8 +240,19 @@ layout (location = 2) in vec3 aFaceNormal;
 
 void main() {
     wsPos = (modelMatrix * vec4(aPos, 1)).xyz;
-    wsVertexNormal = normalMatrix * aVertexNormal;
-    wsFaceNormal = normalMatrix * aFaceNormal;
+    wsVertexNormal = normalize(normalMatrix * aVertexNormal);
+    wsFaceNormal = normalize(normalMatrix * aFaceNormal);
+
+    // workaround for flipped normals in skp
+    vec3 vsPos = (viewMatrix * vec4(wsPos, 1.0)).xyz;
+    vec3 vsVertexNormal = mat3(viewMatrix) * wsVertexNormal;
+    vec3 vsFaceNormal = mat3(viewMatrix) * wsFaceNormal;
+    if (dot(vsPos, vsVertexNormal) > 0) {
+        wsVertexNormal = -wsVertexNormal;
+    }
+    if (dot(vsPos, vsFaceNormal) > 0) {
+        wsFaceNormal = -wsFaceNormal;
+    }
 
     gl_Position = viewProjectionMatrix * modelMatrix * vec4(aPos, 1.0);
 }
